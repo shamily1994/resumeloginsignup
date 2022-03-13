3c7c9fa554e2df04e3e2a3aac55c8e231d91e38b
shamily1994 <shamilysasidharan111@gmail.com>
2022-03-13 16:03:28 +0530

    changed about us text

--- src/components/form/About.js
+++ src/components/form/About.js
@@ -16,12 +16,12 @@ export default function About() {
                           Digital Resume builders
                         </h4>
                 
-                        <p> <span>We are </span>the digital resume building  platform which features the design to help you win your dream job.</p>
-                        <p> <span>Easy online resume builder, </span>Create an awesome resume, online profile without leaving your web browser. </p> 
+                        <p><span>We are </span>the digital resume building  platform. </p>
+                        <p> <span>Easy online resume builder, </span>Create an awesome resume.</p> 
 
                         <p><span>Your data is safe, </span> The data is kept private and protected.</p>
                                 
-                        <p><span>Approved templates, </span>Professionally-designed resume templates. Just edit and download in 5 minutes </p>
+                        <p><span>Approved templates, </span>Professionally-designed resumes.</p>
                         <h2>User-friendly. Professional. Effective. Try our resume builder today!!!</h2>
                         <img className="imgclass3" src="https://media.istockphoto.com/vectors/flat-design-concept-of-our-team-about-us-community-management-vector-id1166406650?k=20&m=1166406650&s=612x612&w=0&h=Ynf0CRHBwMdXgcaXOKlSL1mPeZLQL7we-XJJK-scczk="/>
               {/* <Link to="/back">
